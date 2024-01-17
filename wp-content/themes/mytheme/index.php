2a3d4bddc058afa6c4f9d522b15bd7688d951c18
David Šablatura <davidsablatura@david---macbook-pro-m1.home>
2024-01-17 14:06:57 +0100

    Add header and footer file and modified index about posts

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -7,3 +7,24 @@
  * @package My Theme
  * @since My Theme 1.0
  */
+?>
+<!-- Get Theme Header -->
+    <?php get_header(); ?>
+
+<!-- Get Content -->
+
+    <div class="post-list">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                
+                <article <?php post_class(); ?>>
+                    <h1><?php the_title(); ?></h1>
+
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+
+<!-- Get Theme Footer -->
+    <?php get_footer(); ?>
